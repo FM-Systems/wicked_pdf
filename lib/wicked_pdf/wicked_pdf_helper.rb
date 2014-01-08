f8a2ad314129a79b4e588559ff2e18651b79bee4
Brendan Spinks <brendanspinks@gmail.com>
2014-01-08 14:36:11 +1100

    ability to have a unique css path in public

--- lib/wicked_pdf/wicked_pdf_helper.rb
+++ lib/wicked_pdf/wicked_pdf_helper.rb
@@ -10,7 +10,7 @@ module WickedPdfHelper
   end
 
   def wicked_pdf_stylesheet_link_tag(*sources)
-    css_dir = WickedPdfHelper.root_path.join('public', 'stylesheets')
+    css_dir = WickedPdfHelper.root_path.join('public')
     css_text = sources.collect { |source|
       source = WickedPdfHelper.add_extension(source, 'css')
       "<style type='text/css'>#{File.read(css_dir.join(source))}</style>"
